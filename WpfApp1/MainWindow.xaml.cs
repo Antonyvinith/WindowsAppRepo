db4887a8355df964f3d29f91c76798388a885b59
anton <Antonyvinith>
2024-12-29 16:09:44 +0530

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+
+namespace WpfApp1
+{
+    
+    public partial class MainWindow : Window
+
+        
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Clicked(object Sender,RoutedEventArgs e)
+        {
+            Trace.WriteLine('A Log message here')
+        }
+        
+
+        
+    }
+}
